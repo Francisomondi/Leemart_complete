7882bcd4257718584537c58d17f1f395ee089945
Francis Omondi <francisomondi17@gmail.com>
2024-12-06 16:28:46 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -10,24 +10,17 @@ const callbackHandler = require('./controller/payments/callbackHandler');
 const app = express();
 
 // Middleware to enable CORS
+
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'OPTIONS'],
-    credentials: true
+    origin: 'https://leemart-complete-sbjy.vercel.app', // Allow specific origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Include credentials if needed
 }));
+  
 
 
-export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    // API logic here
-    res.status(200).json({ message: 'Success' });
-  }
+
 app.use(cookieParser())
 
 // Middleware to parse JSON request bodies
